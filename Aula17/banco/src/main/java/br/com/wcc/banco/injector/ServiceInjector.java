6639ed3cfecbd821afbd3cd9b566882949b29b32
Luana Pimentel <luana.pimentel85@gmail.com>
2020-03-21 18:19:03 -0300

    Arrumando injetor de serviços

--- Aula17/banco/src/main/java/br/com/wcc/banco/injector/ServiceInjector.java
+++ Aula17/banco/src/main/java/br/com/wcc/banco/injector/ServiceInjector.java
@@ -1,20 +1,14 @@
 package br.com.wcc.banco.injector;
 
+import br.com.wcc.banco.repositorio.Repositorio;
 import br.com.wcc.banco.servico.Servico;
 
 import java.lang.reflect.InvocationTargetException;
-import java.util.Arrays;
-import java.util.stream.Collectors;
 
 public class ServiceInjector {
-    public <T extends Servico> T getServico(Class<T> clazz, Object... parametros) {
+    public <T extends Servico, R extends Repositorio> T getServico(Class<T> clazz, R repositorio) {
         try {
-            Class[] classes = new Class[parametros.length];
-            Arrays.stream(parametros)
-                    .map(Object::getClass)
-                    .collect(Collectors.toList())
-                    .toArray(classes);
-            return clazz.getDeclaredConstructor(classes).newInstance(parametros);
+            return clazz.getDeclaredConstructor(repositorio.getClass().getInterfaces()[0]).newInstance(repositorio);
         } catch (InstantiationException | IllegalAccessException | InvocationTargetException | NoSuchMethodException e) {
             return null;
         }
